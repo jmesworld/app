bce8b72a8634b81682168f9a021cbfe3876939ce
Hunter Sides <hunter.s.1337@gmail.com>
2023-04-18 09:19:47 -0500

    feat: receive screens implemented

--- src/features/Wallet/index.ts
+++ src/features/Wallet/index.ts
@@ -1,20 +1,19 @@
 import ProfileScreen from './Profile'
 import WalletScreen from './Wallet'
-import WalletSendScreen from './WalletSend'
-import WalletSendConfirmScreen from './WalletSendConfirm'
-import WalletReceiveScreen from './WalletReceive'
-import WalletReceiveConfirmScreen from './WalletReceiveConfirm'
+import SendScreen from './Send'
+import SendConfirmScreen from './SendConfirm'
+import ReceiveScreen from './Receive'
+import RequestScreen from './Request'
 import ScanScreen from './Scan'
 import TransactionHistoryScreen from '../TransactionHistory/TransactionHistory'
-import WalletSendConfirmScreenTest from '../TestScreen'
+
 export {
   ProfileScreen,
   WalletScreen,
-  WalletSendScreen,
-  WalletSendConfirmScreen,
-  WalletSendConfirmScreenTest,
-  WalletReceiveScreen,
-  WalletReceiveConfirmScreen,
+  SendScreen,
+  SendConfirmScreen,
+  ReceiveScreen,
+  RequestScreen,
   ScanScreen,
   TransactionHistoryScreen,
 }
--- src/utils/notateWei.ts
+++ src/utils/notateWei.ts
@@ -3,6 +3,9 @@ export const notateWeiValue = async (amount: number) => {
     notation: 'scientific',
     maximumFractionDigits: 20, // The default is 3, but 20 is the maximum supported by JS according to MDN.
   }
+  // 10e17 is equal to 10 * 10 ** 17, which is equal to 10 ** 18.
+  // Using 10e17 is correct for converting Ether to wei since 1 Ether = 10 ** 18 wei.
+
   const wei = amount * 10e17
   const weiToBigInt = BigInt(wei).toLocaleString('en-us', fmt)
 
